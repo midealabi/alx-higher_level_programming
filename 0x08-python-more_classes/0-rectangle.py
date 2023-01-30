809b138c32224042a2977aeeb4e19d0bd1abea45
midealabi <ayoomide@yahoo.com>
2023-01-30 08:39:51 -0800

    ayo

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/python3 
+"""
+Write an empty class Rectangle that defines a rectangle
+"""
 
 
 class Rectangle:
+    """An empty class Square that defines a square:"""
     pass
